fa52964fd2917cc63e7d50fe78a86d4755007172
rokunyan <reyesroxanejanes@gmail.com>
2024-12-04 21:53:01 +0800

    edited some UI of posting

--- src/components/PostComponents/NewPost.tsx
+++ src/components/PostComponents/NewPost.tsx
@@ -9,26 +9,36 @@ export interface PostData {
   userId: number;
   content: string;
   photo: File | null;
-  isPhotoDeleted? : boolean
+  isPhotoDeleted?: boolean;
 }
 
-const NewPost = ({initialPost, onEdit, photoData}:{initialPost?: PostData, onEdit?:any, photoData?:any}) => {
+const NewPost = ({
+  initialPost,
+  onEdit,
+  photoData,
+}: {
+  initialPost?: PostData;
+  onEdit?: any;
+  photoData?: any;
+}) => {
   const [postUser, setUser] = useState<UserProfile>();
   const username = localStorage.getItem("username");
   const [errors, setErrors] = useState<any>({});
-  const [postData, setPostData] = useState<PostData>(initialPost ?? {
-    userId: 1,
-    content: "",
-    photo: null,
-    isPhotoDeleted: false
-  });
+  const [postData, setPostData] = useState<PostData>(
+    initialPost ?? {
+      userId: 1,
+      content: "",
+      photo: null,
+      isPhotoDeleted: false,
+    }
+  );
   const [showSuccess, setShowSuccess] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
   const [photoImage, setPhotoImage] = useState("");
   const [postButtonStyle, setPostButtonStyle] = useState("");
-  const [imageData, setImageData] = useState("")
-  const [uploadIdHTML, setUploadIdHTML] = useState("icon-button-file")
-  const navigate = useNavigate()
+  const [imageData, setImageData] = useState("");
+  const [uploadIdHTML, setUploadIdHTML] = useState("icon-button-file");
+  const navigate = useNavigate();
 
   useEffect(() => {
     //get user info
@@ -43,15 +53,17 @@ const NewPost = ({initialPost, onEdit, photoData}:{initialPost?: PostData, onEdi
           console.log("Error", error);
         });
     }
-    setPostButtonStyle("w-20 h-6 text-white bg-violet-500 rounded-xl")
-    
-    if(photoData){setImageData(photoData.fileData)}
-    if(initialPost){setUploadIdHTML("icon-button-for-edit")}
+    setPostButtonStyle("w-20 h-6 text-white bg-violet-500 rounded-xl");
 
-
+    if (photoData) {
+      setImageData(photoData.fileData);
+    }
+    if (initialPost) {
+      setUploadIdHTML("icon-button-for-edit");
+    }
   }, []);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setPostData({ ...postData, [name]: value });
   };
@@ -62,21 +74,21 @@ const NewPost = ({initialPost, onEdit, photoData}:{initialPost?: PostData, onEdi
       setPhotoImage(URL.createObjectURL(file));
     }
     setPostData({ ...postData, photo: file });
-    changeStyleWithImage(true)
+    changeStyleWithImage(true);
   };
 
   const handleRemoveImage = () => {
-    setPhotoImage("")
-    setPostData({ ...postData, photo: null, isPhotoDeleted:true });
-    setImageData("")
-    changeStyleWithImage(false)
+    setPhotoImage("");
+    setPostData({ ...postData, photo: null, isPhotoDeleted: true });
+    setImageData("");
+    changeStyleWithImage(false);
   };
 
   // Joi validation schema
   const validationSchema = Joi.object({
     content: Joi.string().required().label("Content"),
     userId: Joi.number().allow().label("UserId"),
-    photo: Joi.any().allow().label("Photo"),   
+    photo: Joi.any().allow().label("Photo"),
     isPhotoDeleted: Joi.boolean().allow().label("IsPhotoDeleted"),
   });
 
@@ -95,15 +107,15 @@ const NewPost = ({initialPost, onEdit, photoData}:{initialPost?: PostData, onEdi
       return;
     }
 
-    if(initialPost){
-      onEdit(postData)
+    if (initialPost) {
+      onEdit(postData);
       return;
     }
 
     try {
       if (postData.content) {
         await postService.addPost(postData);
-        navigate(0)
+        navigate(0);
 
         // Clear form data
         setPostData({
@@ -111,8 +123,8 @@ const NewPost = ({initialPost, onEdit, photoData}:{initialPost?: PostData, onEdi
           content: "",
           photo: null,
         });
-        setPhotoImage("")
-        setImageData("")
+        setPhotoImage("");
+        setImageData("");
 
         // Reset the file input
         const fileInput = document.querySelector(
@@ -131,13 +143,13 @@ const NewPost = ({initialPost, onEdit, photoData}:{initialPost?: PostData, onEdi
     }
   };
 
-  const changeStyleWithImage = (withPhotoImage : boolean) => {
-    if(withPhotoImage){
-      setPostButtonStyle("w-20 h-6 text-white bg-violet-500 rounded-xl")
+  const changeStyleWithImage = (withPhotoImage: boolean) => {
+    if (withPhotoImage) {
+      setPostButtonStyle("w-20 h-6 text-white bg-violet-500 rounded-xl");
     } else {
-      setPostButtonStyle("w-20 h-6 text-white bg-violet-500 rounded-xl")
+      setPostButtonStyle("w-20 h-6 text-white bg-violet-500 rounded-xl");
     }
-  }
+  };
 
   // Close the success message
   const handleCloseSuccess = () => {
@@ -148,22 +160,38 @@ const NewPost = ({initialPost, onEdit, photoData}:{initialPost?: PostData, onEdi
   return (
     <div>
       <div className="bg-gray-50 p-4 rounded-lg border border-gray-200">
-        <div className="flex space-x-3">
-          <div className="flex justify-center items-center">
+        <div className="flex w-full  space-x-3">
+          <div className="flex justify-center items-left h-20">
             <img
               src={`data:image/png;base64,${postUser?.image.fileData}`}
               alt="User Avatar"
               className="w-8 h-8 rounded-full"
             />
           </div>
-          <input
-            type="text"
+          <textarea
+            id="content"
             name="content"
-            value={postData.content}
-            onChange={handleChange}
+            className="w-full p-2 text-sm text-gray-900 bg-white border-1 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
             placeholder="What's on your mind?"
-            className="flex-1 p-2 h-20 text-sm border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
-          />
+            value={postData.content}
+            onChange={(event) => handleChange(event)}
+          ></textarea>
+          {/*
+   <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600"> 
+       <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+           <div className="flex ps-0 space-x-1 rtl:space-x-reverse sm:ps-2">
+               <button type="button" className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                   <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                        <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                    </svg>
+                   <span className="sr-only">Upload image</span>
+               </button>
+           </div>
+           <button type="submit" className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+               Post comment
+           </button>
+       </div>
+   </div>*/}
         </div>
         <div className="flex items-center justify-center mb-4 mt-2">
           {imageData && (
@@ -172,7 +200,7 @@ const NewPost = ({initialPost, onEdit, photoData}:{initialPost?: PostData, onEdi
               alt="Image Data"
               className="mb-2 w-full h-full max-h-100 max-w-100 object-contain rounded-md"
             />
-          )}   
+          )}
           {photoImage && (
             <img
               src={photoImage}
@@ -181,23 +209,22 @@ const NewPost = ({initialPost, onEdit, photoData}:{initialPost?: PostData, onEdi
             />
           )}
         </div>
-        <div className="flex -mt-4 items-center justify-between">
+        <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
           {(photoImage || imageData) && (
             <>
               <button
-                className="w-full border-2 border-gray-100 rounded-xl p-3 bg-transparent"
+                className="inline-flex justify-center items-center text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
                 onClick={handleRemoveImage}
                 id="remove-image-file"
                 style={{ display: "none" }}
               ></button>
               <label htmlFor="remove-image-file" className="flex">
                 <p className="flex text-gray-500 text-sm">
+                <div className="flex ps-0 space-x-1 rtl:space-x-reverse sm:ps-2">
                   <svg
-                    className="w-5 h-5 text-gray-800 dark:text-white"
+                    className="w-4 h-5 mr-2"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
                     fill="none"
                     viewBox="0 0 24 24"
                   >
@@ -208,8 +235,9 @@ const NewPost = ({initialPost, onEdit, photoData}:{initialPost?: PostData, onEdi
                       strokeWidth="2"
                       d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
                     />
-                  </svg>{" "}
-                  Remove Image{" "}
+                  </svg>
+                 Remove Image
+                  </div>
                 </p>
               </label>
             </>
@@ -223,38 +251,25 @@ const NewPost = ({initialPost, onEdit, photoData}:{initialPost?: PostData, onEdi
             onChange={handleImageChange}
           />
           {!photoImage && !imageData && (
-            <label htmlFor={uploadIdHTML} className="flex ml-10">
+            <label htmlFor={uploadIdHTML}>
               <p className="flex text-gray-500 text-sm">
-                <svg
-                  className="w-5 h-5 text-gray-800 dark:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M16 18H8l2.5-6 2 4 1.5-2 2 4Zm-1-8.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z"
-                  />
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M10 3v4a1 1 0 0 1-1 1H5m14-4v16a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V7.914a1 1 0 0 1 .293-.707l3.914-3.914A1 1 0 0 1 9.914 3H18a1 1 0 0 1 1 1ZM8 18h8l-2-4-1.5 2-2-4L8 18Zm7-8.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z"
-                  />
-                </svg>{" "}
-                Upload Image{" "}
-              </p>
+              <div className="flex ps-0 space-x-1 rtl:space-x-reverse sm:ps-2">
+               <button type="button" className="inline-flex justify-center items-center mr-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                   <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                        <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                    </svg>
+               </button>
+             Upload Image  
+           </div>
+           </p>
             </label>
           )}
           {/* Submit button */}
           <div>
-            <button onClick={handleSubmit} className={postButtonStyle}>
-              Post
-            </button>
+
+          <button onClick={handleSubmit} type="submit" className="inline-flex items-center py-2 px-6 text-sm font-medium text-center text-white bg-purple-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-purple-900 hover:bg-purple-800">
+               Post
+           </button>
           </div>
         </div>
       </div>
